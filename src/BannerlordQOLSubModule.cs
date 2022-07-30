ae641d2f3ace2d75420a4dd578d7e7c374af0dc6
Big-Brick <andrij.dzk@gmail.com>
2022-07-30 14:08:41 +0300

    Tmp

--- src/BannerlordQOLSubModule.cs
+++ src/BannerlordQOLSubModule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Windows.Forms;
 using HarmonyLib;
 using TaleWorlds.MountAndBlade;
@@ -18,13 +19,36 @@ namespace BannerlordQOL
 			}
 			catch(Exception ex)
 			{
-				MessageBox.Show("Failed hooking BannerlordQOL attacking code\n\n" + ex.Message);
+				MessageBox.Show("Failed hooking BannerlordQOL code\n\n" + GetString(ex));
 			}
 		}
+
 
 		public static bool OnFinalize(Object __instance)
 		{
 			return true;
+		}		private static string GetString(Exception ex)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+
+			GetStringRecursive(ex, stringBuilder);
+
+			stringBuilder.AppendLine();
+			stringBuilder.AppendLine("Stack trace: ");
+			stringBuilder.AppendLine(ex.StackTrace);
+
+			return stringBuilder.ToString();
+		}
+
+		private static void GetStringRecursive(Exception ex, StringBuilder sb)
+		{
+			sb.AppendLine(ex.GetType().Name + ": ");
+			sb.AppendLine(ex.Message);
+			if (ex.InnerException != null)
+			{
+				sb.AppendLine();
+				GetStringRecursive(ex.InnerException, sb);
+			}
 		}
 	}
 }
